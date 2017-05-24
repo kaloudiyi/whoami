7bd44247461a4668be2d3e9735fbb36439670de0
Kamal Loudiyi <kaloudiyi@yahoo.fr>
2017-05-24 17:55:15 +0200

    try platform name

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 var express = require('express');
+var dns = require('dns');
 var app = express();
 
 const PORT = process.env.PORT || 3000;
-console.log('Up on the port '+PORT); 
+console.log('Up on the port ' + PORT);
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-if (ip.substr(0, 7) == "::ffff:") {
-  ip = ip.substr(7)
-}
+  if (ip.substr(0, 7) == "::ffff:") {
+    ip = ip.substr(7)
+  }
+  dns.reverse(req.connection.remoteAddress, function(err, domains) {
+    res.send(JSON.stringify(domains));
+  });
   var ret = {
-     ipadress: ip,
-     language: req.headers["accept-language"].split(",")[0].toLowerCase()
+    ipadress: ip,
+    language: req.headers["accept-language"].split(",")[0].toLowerCase()
   };
   res.send(JSON.stringify(ret));
 });
