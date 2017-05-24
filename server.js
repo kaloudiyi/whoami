132e11ae0911ef54b73d1270c973e20396522718
Kamal Loudiyi <kaloudiyi@yahoo.fr>
2017-05-24 17:16:17 +0200

    remove comments

--- server.js
+++ server.js
@@ -1,22 +1,3 @@
-/* const http = require('http');
-const os = require('os');
-
-const PORT = process.env.PORT || 3000;
-console.log('Up on the port '+PORT);
-
-
-const server = http.createServer((req, res) => {
-
-  // IP Adress looking for
-  console.log(req.headers["x-forwarded-for"]);
-  var language = req.headers["accept-language"].split(",")[0];
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.write("language:"+language+"\n");
-  res.end();
-}).listen(PORT);
-
-*/
 var express = require('express');
 var app = express();
 
