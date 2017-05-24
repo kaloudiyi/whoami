ed36486f8e4fbba4fa5090d41b036ea697f456ee
Kamal Loudiyi <kaloudiyi@yahoo.fr>
2017-05-24 18:33:59 +0200

    Maybe the final version

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
 var express = require('express');
-var dns = require('dns');
 var app = express();
 
 const PORT = process.env.PORT || 3000;
 console.log('Up on the port ' + PORT);
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
+  // get the ip 
   var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   if (ip.substr(0, 7) == "::ffff:") {
     ip = ip.substr(7)
   }
-  dns.reverse(ip,function(err, domains) {
-    res.send(JSON.stringify(domains));
-  });
-  /*var ret = {
+ // get the software
+ var regExp = /\(([^)]+)\)/;
+ var matches = regExp.exec(req.headers['user-agent']);
+ // create the object
+ var ret = {
     ipadress: ip,
-    language: req.headers["accept-language"].split(",")[0].toLowerCase()
+    language: req.headers["accept-language"].split(",")[0].toLowerCase(),
+    software: matches[1]
   };
-  res.send(JSON.stringify(ret)); */
+  res.send(JSON.stringify(ret));
 });
 
 app.listen(PORT);
