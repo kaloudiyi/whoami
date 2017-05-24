8602b3a81a9b8c4b7d5af4feb2f183f648de3fe4
Kamal Loudiyi <kaloudiyi@yahoo.fr>
2017-05-24 17:26:14 +0200

    try to remove the ffff:: display

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ console.log('Up on the port '+PORT);
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+if (ip.substr(0, 7) == "::ffff:") {
+  ip = ip.substr(7)
+}
   var ret = {
-     ipadress: req.ip,
+     ipadress: ip,
      language: req.headers["accept-language"].split(",")[0].toLowerCase()
   };
   res.send(JSON.stringify(ret));
